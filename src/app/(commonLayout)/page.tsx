7690b7abbaa3a2c22510c3a95def9a3d61ebd7c4
Emon Hossain <islamemon59@gmail.com>
2026-01-18 23:26:06 +0600

    session set on cookie from server

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
+  const cookieStore = await cookies();
+  // console.log(cookieStore);
 
-  const getSession = await authClient.getSession();
-  console.log(getSession);
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+  const session = await res.json();
+  console.log("Get Session", session);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button variant="outline" size="lg">Click Me</Button>
+      <Button variant="outline" size="lg">
+        Click Me
+      </Button>
     </div>
   );
 }
