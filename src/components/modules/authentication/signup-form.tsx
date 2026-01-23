daf9039503d20b425e6f7ab863f5d3fa1339cea8
Emon Hossain <islamemon59@gmail.com>
2026-01-23 22:27:42 +0600

    tsnstack form added

--- src/components/modules/authentication/signup-form.tsx
+++ src/components/modules/authentication/signup-form.tsx
@@ -1,20 +1,22 @@
+"use clint"
 import { Button } from "@/components/ui/button"
+// import { useForm } from "@tanstack/react-form"
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  // const form = useForm({
+  //   defaultValues: {
+      
+  //   }
+  // })
   return (
     <Card {...props}>
       <CardHeader>
@@ -24,53 +26,13 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="name">Full Name</FieldLabel>
-              <Input id="name" type="text" placeholder="John Doe" required />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-              <FieldDescription>
-                We&apos;ll use this to contact you. We will not share your email
-                with anyone else.
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password" type="password" required />
-              <FieldDescription>
-                Must be at least 8 characters long.
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="confirm-password">
-                Confirm Password
-              </FieldLabel>
-              <Input id="confirm-password" type="password" required />
-              <FieldDescription>Please confirm your password.</FieldDescription>
-            </Field>
-            <FieldGroup>
-              <Field>
-                <Button type="submit">Create Account</Button>
-                <Button variant="outline" type="button">
-                  Sign up with Google
-                </Button>
-                <FieldDescription className="px-6 text-center">
-                  Already have an account? <a href="#">Sign in</a>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </FieldGroup>
+        <form id="orderForm">
         </form>
       </CardContent>
+      <CardFooter className="flex justify-end">
+          <Button form="orderForm" type="submit">Submit</Button>
+
+      </CardFooter>
     </Card>
   )
 }
